e822aad13389bf80769cc281c1feae746f940db2
Prashant Bansal <109963029+prashantb088@users.noreply.github.com>
2022-09-01 18:19:43 +0530

    Create laptop.java

---
+++ laptop.java
@@ -0,0 +1,27 @@
+
+
+
+
+package com.bankApp;
+
+public class laptop {
+
+
+    int lid;
+    String lCompany;
+    String lName;
+    int lPrice;
+    int lModel;
+    public laptop(int lid, String lCompany,String lName,int lPrice,int lModel){
+        this. lid = lid;
+        this.lCompany = lCompany;
+        this.lName = lName;
+        this.lPrice = lPrice;
+        this.lModel = lModel;
+
+
+    }
+
+
+
+}
